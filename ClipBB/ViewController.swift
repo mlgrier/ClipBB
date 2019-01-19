d7b4241a15d73c736df7221946880cd3d7469248
Marco Grier <mlgrier@gmail.com>
2019-01-19 10:52:08 -0600

    Added drawing line and clear option

--- ClipBB/ViewController.swift
+++ ClipBB/ViewController.swift
@@ -11,86 +11,59 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var mainImage: UIImageView!
-    @IBOutlet weak var tempImage: UIImageView!
+    @IBOutlet weak var clearButton: UIButton!
     
-    var lastPoint = CGPoint.zero
-    var color = UIColor.black
-    var brushWidth: CGFloat = 10.0
-    var opacity: CGFloat = 1.0
-    var swiped = false
+    var path = UIBezierPath()
+    var startPoint = CGPoint()
+    var touchPoint = CGPoint()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setup()
+        
     }
     
+    func setup() {
+        clearButton.layer.cornerRadius = 20
+        mainImage.clipsToBounds = true
+        mainImage.isMultipleTouchEnabled = false
+    }
+   
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {
-            return
+        let touch = touches.first
+        if let point = touch?.location(in: mainImage) {
+            startPoint = point
         }
-        swiped = false
-        lastPoint = touch.location(in: view)
-    }
-    
-    func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
-        // 1
-        UIGraphicsBeginImageContext(view.frame.size)
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return
-        }
-        tempImage.image?.draw(in: view.bounds)
-        
-        // 2
-        context.move(to: fromPoint)
-        context.addLine(to: toPoint)
-        
-        // 3
-        context.setLineCap(.round)
-        context.setBlendMode(.normal)
-        context.setLineWidth(brushWidth)
-        context.setStrokeColor(color.cgColor)
-        
-        // 4
-        context.strokePath()
-        
-        // 5
-        tempImage.image = UIGraphicsGetImageFromCurrentImageContext()
-        tempImage.alpha = opacity
-        UIGraphicsEndImageContext()
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {
-            return
+        let touch = touches.first
+        if let point = touch?.location(in: mainImage) {
+            touchPoint = point
         }
+        path.move(to: startPoint)
+        path.addLine(to: touchPoint)
+        startPoint = touchPoint
         
-        // 6
-        swiped = true
-        let currentPoint = touch.location(in: view)
-        drawLine(from: lastPoint, to: currentPoint)
-        
-        // 7
-        lastPoint = currentPoint
+        draw()
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if !swiped {
-            // draw a single point
-            drawLine(from: lastPoint, to: lastPoint)
-        }
-        
-        // Merge tempImageView into mainImageView
-        UIGraphicsBeginImageContext(mainImage.frame.size)
-        mainImage.image?.draw(in: view.bounds, blendMode: .normal, alpha: 1.0)
-        tempImage?.image?.draw(in: view.bounds, blendMode: .normal, alpha: opacity)
-        mainImage.image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        tempImage.image = nil
+    func draw() {
+        let strokeLayer = CAShapeLayer()
+        strokeLayer.fillColor = nil
+        strokeLayer.lineWidth = 5
+        strokeLayer.strokeColor = UIColor.black.cgColor
+        strokeLayer.path = path.cgPath
+        mainImage.layer.addSublayer(strokeLayer)
+        mainImage.setNeedsDisplay()
     }
 
-
-
-
+    @IBAction func clearAction(_ sender: Any) {
+        path.removeAllPoints()
+        mainImage.layer.sublayers = nil
+        mainImage.setNeedsDisplay()
+    }
+    
 }
 
