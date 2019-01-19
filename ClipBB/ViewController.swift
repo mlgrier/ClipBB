7c13f39d3b89fb6aba6b0b99b4587b119a42593f
Marco Grier <mlgrier@gmail.com>
2019-01-19 07:39:48 -0600

    Initial Commit

---
+++ ClipBB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClipBB
+//
+//  Created by Marco Grier on 1/19/19.
+//  Copyright © 2019 Hoodde LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
